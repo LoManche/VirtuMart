4a7dd0f2c638787d1fd1f350644c6ae1ef201975
LoManche <manlo648@gmail.com>
2024-03-16 16:47:15 +0800

    initalized database.js, server.js, .env

---
+++ VirtuMart/server/database.js
@@ -0,0 +1,26 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connection = mysql.createConnection({
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PW,
+  database : process.env.DB_NAME
+}).promise(); //allows use of async/await
+ 
+connection.connect(function (err){
+  if (err){
+    console.error('Error connecting:' + err.stack)
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
+
+// connection.query(sqlString, values, callback);
+// connection.query('SELECT * FROM users WHERE id = ?', [userId], function (error, results, fields) {
+//   if (error) throw error;
+//   ...
+// });
+
+connection.end();
---
+++ VirtuMart/server/server.js
@@ -0,0 +1,23 @@
+import React from "react";
+//import { renderToString } from "react-dom/server";
+//import { StaticRouter } from "react-router-dom/server";
+import express from "express";
+import session from "express-session";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+	extended: true
+}));
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+
