8c151f1dcfac826f8ab27023885cf1eef93b2058
LoManche <manlo648@gmail.com>
2024-03-16 15:51:36 +0800

    Correctly added prev packages, deleted prev wrong installs

--- server.js
+++ server.js
@@ -22,14 +22,22 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'me',
   password : 'secret',
-  database : 'my_db'
+  database : 'VirtuMartDb'
 });
  
-connection.connect();
+connection.connect(function (err){
+  if (err){
+    console.error('Error connecting:' + err.stack)
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
  
-// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+// connection.query(sqlString, values, callback);
+// connection.query('SELECT * FROM users WHERE id = ?', [userId], function (error, results, fields) {
 //   if (error) throw error;
-//   console.log('The solution is: ', results[0].solution);
+//   ...
 // });
- 
+
 connection.end();
+
