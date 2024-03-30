72521f415e52fab580f3f69f2ba3917b51b9c28f
LoManche <manlo648@gmail.com>
2024-03-30 20:53:43 +0800

    updated init sql

--- db_setup(not completed)/initializingDB.sql
+++ db_setup(not completed)/initializingDB.sql
@@ -2,30 +2,46 @@
 create database if not exist virtumartdb;
 use virtumartdb;
 SET global local_infile = 1;
+-- Creating tables
 create table if not exist products (
-	asin varchar(10) NOT NULL,
-    title varchar(255),
-    imgURL varchar(255),
-	stars float,
-    price float,
-    listPrice float,
-    category_id int,
-    primary key (asin)
+  asin varchar(10) NOT NULL,
+  title varchar(255),
+  imgURL varchar(255),
+  rating float,
+  price float,
+  discount float,
+  category_id int,
+  description varchar(255),
+  stock int,
+  primary key (asin)
 );
 create table if not exist customers (
-	id int not null auto_increment,
-    name varchar(20) not null,
-    primary key (id)
+	customer_id int not null auto_increment,
+  username varchar(20) not null,
+  password varchar(20) not null,
+  email varchar(255) not null,
+  cart_id int not null,
+  primary key (id)
+  foreign key (cart_id) references shopping_cart(cart_id)
+);
+create table if not exist shopping_cart (
+  cart_id int not null auto_increment,
+  customer_id int not null,
+  product_id varchar(10) not null,
+  quantity int not null,
+  primary key (cart_id),
+  foreign key (customer_id) references customers(customer_id),
+  foreign key (product_id) references products(asin)
 );
 create table if not exist reviews (
 	review_id int not null auto_increment,
-    product_id varchar(10) not null,
-    star int not null,
-    customer_id int not null,
-    content varchar(255) not null,
-    primary key (review_id),
-    foreign key (product_id) references products(asin),
-    foreign key (customer_id) references customers(id)
+  product_id varchar(10) not null,
+  rating int not null,
+  customer_id int not null,
+  review varchar(255) not null,
+  primary key (review_id),
+  foreign key (product_id) references products(asin),
+  foreign key (customer_id) references customers(customer_id)
 );
 create table if not exist categories (
     category_id int not null,
@@ -33,6 +49,28 @@ create table if not exist categories (
     primary key (category_id)
 );
 
+create table if not exist order(
+    order_id int not null auto_increment,
+    customer_id int not null,
+    subTotal float not null,
+    shipping float not null,
+    dataOfOrder date not null,
+    primary key (order_id),
+    foreign key (customer_id) references customers(customer_id),
+);
+
+create table if not exist order_products (
+    id int not null auto_increment,
+    order_id int not null,
+    customer_id int not null,
+    product_id int[] not null,
+    quantity int not null,
+    primary key (id),
+    foreign key (order_id) references order(order_id),
+    foreign key (customer_id) references customers(customer_id),
+    foreign key (product_id) references products(asin)
+);
+-- Inserting data from csv files
 LOAD DATA LOCAL INFILE 'G:/Codes/3100Project/amazon_products_sample_ansi.csv' 
 INTO TABLE products 
 FIELDS TERMINATED BY ',' 
@@ -48,3 +86,4 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS 
 (category_id,category_name);
+-- Inserting data manually
