c0da4e28c6296634a217078652bbec8b691808c2
cloudarcher <happyngtiffany02@gmail.com>
2024-04-09 22:00:21 +0800

    add /api path to api.js

--- VirtuMart/src/api.jsx
+++ VirtuMart/src/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 axios.defaults.method = "POST";
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "http://localhost:3000/api";
 axios.defaults.headers.common["Content-Type"] = "application/json";
 axios.defaults.withCredentials = true;
 export class ServerError extends Error {
