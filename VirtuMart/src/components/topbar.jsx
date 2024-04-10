1362173362d71ea46712d7ad8ffeb407ad8e38c9
cloudarcher <happyngtiffany02@gmail.com>
2024-04-11 07:36:30 +0800

    add notification function

--- VirtuMart/src/components/topbar.jsx
+++ VirtuMart/src/components/topbar.jsx
@@ -16,33 +16,67 @@ import {
   IconButton,
   MenuItem,
   Menu,
+  Divider,
   Badge,
   Button,
   InputAdornment,
   TextField,
   ListItemIcon,
+  Modal,
+  Typography,
 } from "@mui/material";
 import { useNavigate } from "react-router";
 import virtumartLogo from "../assets/VirtuMartLogo.png";
 import Api from "../api";
 import { useAppContext } from "../contexts/appContext";
+import handleError from "./handleError";
 
 export default function Topbar({ type }) {
   const navigate = useNavigate();
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-  const badgeContent = 0;
+  const [alert, setAlert] = React.useState([
+    // { dateOfNotification: "11/4/2024", message: "You have ordered a product." },
+    // { dateOfNotification: "11/4/2024", message: "You have ordered a product." },
+  ]);
+
+  const badgeContent = alert.length;
   const { isLogin, setIsLogin, user, setUser } = useAppContext();
   const role = user?.role;
   const [searchInput, setSearchInput] = React.useState(localStorage.getItem("searchInput"));
 
+  const [alertMenu, setAlertMenu] = React.useState(null);
+  const open = Boolean(alertMenu);
   const onSubmit = (e) => {
     if (e.key === "Enter") {
       localStorage.setItem("searchInput", searchInput);
       navigate("/result");
     }
   };
+
+  const handleAlertMenuClose = () => {
+    setAlertMenu(null);
+    setAlert([]);
+  };
+
+  const handleAlertMenuOpen = (event) => {
+    setAlertMenu(event.currentTarget);
+  };
+
+  React.useEffect(() => {
+    const loadData = async () => {
+      try {
+        const alert = await Api.notification();
+        setAlert(alert);
+      } catch (err) {
+        handleError(err, () => {}, true);
+        throw err;
+      }
+    };
+    loadData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const logout = async () => {
     await Api.logout();
@@ -81,6 +115,29 @@ export default function Topbar({ type }) {
     );
   }
 
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const [openModal, setOpenModal] = React.useState(false);
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+  const handleCloseModal = () => {
+    setOpenModal(false);
+    setAlert([]);
+  };
+
+  //  <Button onClick={handleOpen}>Open modal</Button>
+
   const mobileMenuId = "primary-search-account-menu-mobile";
   const renderMobileMenu = (
     <Menu
@@ -100,6 +157,98 @@ export default function Topbar({ type }) {
       sx={{
         display: { xs: "block", md: "none" },
       }}>
+      <MenuItem
+        onClick={(e) => {
+          e.preventDefault();
+          handleOpenModal(e);
+        }}>
+        <ListItemIcon>
+          {badgeContent === 0 ? (
+            <NotificationsIcon />
+          ) : (
+            <Badge badgeContent={badgeContent} color="error">
+              <NotificationsIcon />
+            </Badge>
+          )}
+        </ListItemIcon>
+        <p>Notification</p>
+      </MenuItem>
+      <Modal
+        open={openModal}
+        onClose={handleCloseModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description">
+        <Box sx={style}>
+          {alert.length > 0 ? (
+            alert.map((alert, key) => {
+              return (
+                <>
+                  <Typography id="modal-modal-title" variant="h6" component="h2" mt={1}>
+                    {alert.dateOfNotification}
+                  </Typography>
+                  <Typography id="modal-modal-description" mb={1}>
+                    {alert.message}
+                  </Typography>
+                  <Divider />
+                </>
+              );
+            })
+          ) : (
+            <Typography>No Notification</Typography>
+          )}
+        </Box>
+      </Modal>
+      <Menu
+        sx={{ mt: "45px" }}
+        id="menu-appbar"
+        anchorEl={anchorElUser}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        open={Boolean(anchorElUser)}
+        onClose={handleCloseUserMenu}>
+        {role === "admin" ? (
+          <MenuItem
+            onClick={() => {
+              handleCloseUserMenu();
+              navigate("/admin");
+            }}>
+            <ListItemIcon>
+              <ManageAccountsIcon />
+            </ListItemIcon>
+            <p>Management</p>
+          </MenuItem>
+        ) : (
+          <></>
+        )}
+        <MenuItem
+          onClick={() => {
+            handleCloseUserMenu();
+            navigate(isLogin ? "/profile" : "/account");
+          }}>
+          <ListItemIcon>
+            <AccountCircle />
+          </ListItemIcon>
+          <p>{isLogin ? "Profile" : "Sign In / Sign Up"}</p>
+        </MenuItem>
+        {isLogin ? (
+          <MenuItem onClick={logout}>
+            <ListItemIcon>
+              <LogoutIcon />
+            </ListItemIcon>
+            <p>Sign Out</p>
+          </MenuItem>
+        ) : (
+          <></>
+        )}
+      </Menu>
+
       <MenuItem
         onClick={() => {
           handleMobileMenuClose();
@@ -216,7 +365,13 @@ export default function Topbar({ type }) {
               </Box>
             </Box>
             <Box sx={{ display: { xs: "none", sm: "flex", md: "flex" } }}>
-              <IconButton size="large" color="inherit" onClick={() => {}}>
+              <IconButton
+                size="large"
+                color="inherit"
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleAlertMenuOpen(e);
+                }}>
                 {badgeContent === 0 ? (
                   <NotificationsIcon />
                 ) : (
@@ -225,6 +380,59 @@ export default function Topbar({ type }) {
                   </Badge>
                 )}
               </IconButton>
+              <Menu
+                anchorEl={alertMenu}
+                id="account-menu"
+                open={open}
+                onClose={handleAlertMenuClose}
+                onClick={handleAlertMenuClose}
+                PaperProps={{
+                  elevation: 0,
+                  sx: {
+                    overflow: "visible",
+                    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                    mt: 1.5,
+                    "& .MuiAvatar-root": {
+                      width: 32,
+                      height: 32,
+                      ml: -0.5,
+                      mr: 1,
+                    },
+                    "&::before": {
+                      content: '""',
+                      display: "block",
+                      position: "absolute",
+                      top: 0,
+                      right: 14,
+                      width: 10,
+                      height: 10,
+                      bgcolor: "background.paper",
+                      transform: "translateY(-50%) rotate(45deg)",
+                      zIndex: 0,
+                    },
+                  },
+                }}
+                transformOrigin={{ horizontal: "right", vertical: "top" }}
+                anchorOrigin={{ horizontal: "right", vertical: "bottom" }}>
+                {alert.length > 0 ? (
+                  alert.map((alert, key) => {
+                    return (
+                      <>
+                        <MenuItem key={key} disabled>
+                          <Typography>
+                            {alert.dateOfNotification + " "} {alert.message}
+                          </Typography>
+                        </MenuItem>
+                        <Divider />
+                      </>
+                    );
+                  })
+                ) : (
+                  <MenuItem disabled>
+                    <Typography>No Notification</Typography>
+                  </MenuItem>
+                )}
+              </Menu>
               <ICONBUTTON icon={<ShoppingCartIcon />} navigateTo={"/shoppingCart"} />
               <IconButton size="large" color="inherit" onClick={handleOpenUserMenu}>
                 <AccountCircle />
