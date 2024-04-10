8a78165928a27d696ad264fc23e5282653313e3a
king0315746 <www.chankinghei@gmail.com>
2024-04-10 14:44:41 +0800

    finished cart page

---
+++ VirtuMart/src/components/productInstance.jsx
@@ -0,0 +1,47 @@
+import Checkbox from "@mui/material/Checkbox";
+import { TextField, Button } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+export default function ProductInstance({ img, title, price }) {
+  return (
+    <div>
+      <div style={{ display: "flex", marginLeft: "100px", marginRight: "10px", width: "600px" }}>
+        <div>
+          <Checkbox
+            sx={{
+              "&.Mui-checked": {
+                color: "#1191D9",
+              },
+              marginTop: "90px",
+            }}
+          />
+        </div>
+        <div>
+          <img
+            src={img}
+            style={{ maxHeight: "200px", width: "300px", borderRadius: "5px", margin: "30px" }}
+          />
+        </div>
+        <div style={{ width: "200px" }}>
+          <h1 style={{ marginBottom: "1px", width: "200px" }}>{title}</h1>
+          <p style={{ margin: "1px 0px 1px 0px" }}>{price}</p>
+          <TextField style={{ width: "40px", margin: "7px" }} defaultValue="1" size="small" />
+          <p style={{ color: "#06B122", margin: "1px 0px 1px 0px" }}>In Stock</p>
+        </div>
+        <div>
+          <IconButton aria-label="delete" style={{ margin: "90px 10px 70px 30px" }}>
+            <DeleteIcon />
+          </IconButton>
+        </div>
+      </div>
+      <hr
+        style={{
+          borderTop: "1px solid #E0E0E0",
+          margin: "0 30px 0 0",
+          width: "800px",
+          textAlign: "left",
+        }}></hr>
+    </div>
+  );
+}
--- VirtuMart/src/pages/shoppingCart.jsx
+++ VirtuMart/src/pages/shoppingCart.jsx
@@ -1,53 +1,235 @@
-import { Carousel } from "react-responsive-carousel";
+import ProductInstance from "../components/productInstance";
+import ProductCarousel from "../components/productCarousel";
+import { TextField, Button } from "@mui/material";
+import Checkbox from "@mui/material/Checkbox";
 
-function ProductShow({ thumbnails }) {
+function DefaultAddress() {
   return (
-    <Carousel showArrows={true} autoplay={true} infiniteLoop={true}>
+    <div style={{ display: "flex" }}>
+      <Checkbox
+        sx={{
+          "&.Mui-checked": {
+            color: "#1191D9",
+          },
+          marginTop: "90px",
+        }}
+      />
       <div>
-        <img src={thumbnails[0].src} alt={thumbnails[0].alt} />
+        <p>29C, Virtual Building, Virtual Street</p>
+        <p>Virtual City</p>
+        <p>Virtual Country</p>
       </div>
-      <div>
-        <img src={thumbnails[1].src} alt={thumbnails[1].alt} />
-      </div>
-      <div>
-        <img src={thumbnails[2].src} alt={thumbnails[2].alt} />
-      </div>
-      <div>
-        <img src={thumbnails[3].src} alt={thumbnails[3].alt} />
-      </div>
-      {/*
-      {thumbnails.map((item, index) => (
-        <div>
-          <img src={item[index].src} alt={item[index].alt} />
-        </div>
-      ))}
-    */}
-    </Carousel>
+    </div>
   );
 }
 
+function AddressInput() {
+  return (
+    <div style={{ display: "flex" }}>
+      <Checkbox
+        sx={{
+          "&.Mui-checked": {
+            color: "#1191D9",
+          },
+          marginTop: "90px",
+        }}
+      />
+      <div>
+        <TextField
+          id="address"
+          label="Address"
+          variant="outlined"
+          style={{ padding: "5px", width: "450px" }}
+        />
+        <TextField
+          id="city"
+          label="City"
+          variant="outlined"
+          style={{ padding: "5px", width: "150px" }}
+        />
+        <TextField
+          id="country"
+          label="Country"
+          variant="outlined"
+          style={{ padding: "5px", width: "150px" }}
+        />
+        <TextField
+          id="postalCode"
+          label="Postal Code"
+          variant="outlined"
+          style={{ padding: "5px", width: "150px" }}
+        />
+      </div>
+    </div>
+  );
+}
+
+function Subtotal(price, subtotal) {
+  // Loop through popularProducts array
+  for (let i = 0; i < 3; i++) {
+    // Access each product and add its price to the total
+    subtotal += price;
+  }
+
+  return subtotal;
+}
+
 export default function ShoppingCart() {
-  const thumbnails = [
+  const productID = "1";
+  let subtotal = 0;
+  const Products = [
     {
-      src: "https://cdn.builder.io/api/v1/image/assets/TEMP/fe41f674ed35f646b78ca021b755863e91a119564425ee799d6e9f7e2e5fa3e6?apiKey=64e0584885e94e77ae2d2a5ac36293f7&",
-      alt: "Thumbnail 1",
+      img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+      title: "Basketball",
+      price: 70,
+      originalPrice: "$100",
+      productID: 1,
+      description: "@tjdragotta",
+      quantity: "1",
     },
     {
-      src: "https://cdn.builder.io/api/v1/image/assets/TEMP/61e2caebbac60173c56ab6870677863e2f513976fe9049844baef3473d93064d?apiKey=64e0584885e94e77ae2d2a5ac36293f7&",
-      alt: "Thumbnail 2",
+      img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+      title: "Fern",
+      price: 50,
+      productID: 1,
+      description: "@katie_wasserman",
+      quantity: "1",
     },
     {
-      src: "https://cdn.builder.io/api/v1/image/assets/TEMP/8496a77b109dc5df028507ef3efeb1328de198d0da4625c744d970765dc1d3c3?apiKey=64e0584885e94e77ae2d2a5ac36293f7&",
-      alt: "Thumbnail 3",
+      img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+      title: "Mushrooms",
+      price: 10,
+      productID: 1,
+      description: "@silverdalex",
+      quantity: "1",
     },
     {
-      src: "https://cdn.builder.io/api/v1/image/assets/TEMP/fedcdbd43dbee31cf010d2ccb5afc3f81ca5210a3f4e4a24ef7dd42b9bbbf215?apiKey=64e0584885e94e77ae2d2a5ac36293f7&",
-      alt: "Thumbnail 4",
+      img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+      title: "Tomato basil",
+      price: 15,
+      productID: 1,
+      description: "@shelleypauls",
+      quantity: "1",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+      title: "Sea star",
+      price: 30,
+      productID: 1,
+      description: "@peterlaster",
+      quantity: "1",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+      title: "Bike",
+      price: 500,
+      productID: 1,
+      description: "@southside_customs",
+      quantity: "1",
     },
   ];
+
+  for (let i = 0; i < Products.length; i++) {
+    subtotal += Products[i].price;
+  }
+
   return (
     <div>
-      <ProductShow thumbnails={thumbnails}></ProductShow>
+      {/*header*/}
+      <div style={{ display: "flex" }}>
+        <h1 style={{ margin: "0 10px 5px 100px", width: "700px" }}>Shopping Cart</h1>
+        <div style={{ textAlign: "right", width: "700px" }}>
+          <Button
+            style={{
+              backgroundColor: "black",
+              color: "white",
+              width: "120px",
+              borderRadius: "8px",
+              margin: "0 5px 0 5px",
+              height: "40px",
+              width: "200px",
+            }}>
+            Delete Selected
+          </Button>
+          <Button
+            style={{
+              backgroundColor: "black",
+              color: "white",
+              width: "120px",
+              borderRadius: "8px",
+              margin: "0 5px 0 5px",
+              height: "40px",
+              width: "200px",
+            }}>
+            Delete All
+          </Button>
+        </div>
+      </div>
+      <hr style={{ borderTop: "2px solid black", margin: "0 0" }}></hr>
+      {/*content*/}
+      <div style={{ display: "flex" }}>
+        <div>
+          {Products.map((Product, index) => (
+            <div key={index} style={{ padding: "3px" }}>
+              <ProductInstance img={Product.img} title={Product.title} price={Product.price} />
+            </div>
+          ))}
+        </div>
+        <hr></hr> {/*vertical divider*/}
+        <div style={{ marginLeft: "30px" }}>
+          <div>
+            <DefaultAddress></DefaultAddress>
+          </div>
+          <div>
+            <AddressInput></AddressInput>
+          </div>
+          <hr
+            style={{
+              borderTop: "1px solid #E0E0E0",
+              margin: "10px 30px 0 0",
+              width: "500px",
+              textAlign: "left",
+            }}></hr>
+          <div style={{ display: "flex", width: "400px" }}>
+            <div style={{ width: "200px" }}>
+              {Products.map((Product, index) => (
+                <p key={index}>
+                  {Product.title} *{Product.quantity}
+                </p>
+              ))}
+            </div>
+            <div style={{ textAlign: "right", width: "200px" }}>
+              {Products.map((Product, index) => (
+                <p key={index}>${Product.price.toFixed(2)}</p>
+              ))}
+            </div>
+          </div>
+          <hr
+            style={{
+              borderTop: "1px solid #E0E0E0",
+              margin: "10px 30px 0 0",
+              width: "500px",
+              textAlign: "left",
+            }}></hr>
+          <div style={{ textAlign: "right", width: "400px" }}>
+            <h3>Subtotal: ${subtotal.toFixed(2)}</h3>
+          </div>
+          <Button
+            style={{
+              backgroundColor: "black",
+              color: "white",
+              width: "120px",
+              borderRadius: "8px",
+            }}>
+            Checkout
+          </Button>
+        </div>
+      </div>
+      {/*Bottom*/}
+      <div>
+        <h2>Frequently bought together</h2>
+        <ProductCarousel Products={Products}></ProductCarousel>
+      </div>
     </div>
   );
 }
